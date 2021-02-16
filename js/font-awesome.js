93db286b8cc5b489c3675a0a901e4801590cd248
Matthew Roberts <matt@mroberts.me>
2021-02-16 12:52:39 -0500

    shatt public site

---
+++ js/font-awesome.js
@@ -0,0 +1,8 @@
+import fontawesome from './fontawesome/fontawesome/index.js'
+import regular from './fontawesome/fontawesome-free-regular/index.js'
+import solid from './fontawesome/fontawesome-free-solid/index.js'
+import brands from './fontawesome/fontawesome-free-brands/index.js'
+
+fontawesome.library.add(regular)
+fontawesome.library.add(solid)
+fontawesome.library.add(brands)
